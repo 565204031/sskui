99faa04f2f51848ddc4323f345f1fc0e3f70c395
565204031 <565204031@qq.com>
2015-08-13 15:37:27 +0800

    修改ViewUtils
    修改ViewUtils

--- src/com/ssk/sskui/utils/ViewUtils.java
+++ src/com/ssk/sskui/utils/ViewUtils.java
@@ -9,8 +9,11 @@ public class ViewUtils {
 
 	public static void initViews(Object activity)
 	{
+		initViews(activity,null);
+	}
+	public static void initViews(Object activity,Object view){
 		//类模板
-		Class cls=activity.getClass();
+		Class<?> cls=activity.getClass();
 		//获得字段getDeclaredFields包括public、private和proteced
 		//getFields只能是public
 		Field[] fields=cls.getDeclaredFields();
@@ -19,7 +22,7 @@ public class ViewUtils {
 		for(Field f:fields)
 		{
 			//获得字段类型
-			Class ctype=f.getType();
+			Class<?> ctype=f.getType();
 			//继承View的字段
 			if(View.class.isAssignableFrom(ctype))
 			{
@@ -27,22 +30,31 @@ public class ViewUtils {
 				f.setAccessible(true);
 				try {
 					//拿到调用方法
-					Method findviewbyid=cls.getMethod("findViewById",new Class[]{int.class});
-					
-					Class idclass=R.id.class;
+					Method findviewbyid;
+					if(view!=null){
+						Class<?> clsview=view.getClass();
+						findviewbyid=clsview.getMethod("findViewById",new Class[]{int.class});
+					}else{
+						findviewbyid=cls.getMethod("findViewById",new Class[]{int.class});
+					}
+					Class<?> idclass=R.id.class;
 					
 					Field fid=idclass.getDeclaredField(f.getName());
 					//静态方法，不需要传对象，所以随便搞一个
 					Object obj=fid.get(fid);
 					//invoke(对象，参数) 参数在R里面,所以要反射R
-					Object value=findviewbyid.invoke(activity,new Object[]{obj});
+					Object value;
+					if(view!=null){
+						value=findviewbyid.invoke(view,new Object[]{obj});
+					}else{
+						value=findviewbyid.invoke(activity,new Object[]{obj});
+					}
 					//对象，返回值
 					f.set(activity, value);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
 			}
 		}
 	}
